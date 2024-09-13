6366c1107a0e0476394a7a61013847090d5f4bd3
Antonio <tonofalcon22@gmail.com>
2024-09-12 20:27:12 -0600

    first commit

---
+++ js/music_list.js
@@ -0,0 +1,422 @@
+const music_list = [
+    {
+        img : 'images/m1.png',
+        name : 'Bomb Man',
+        artist : 'Megaman 1',
+        music : 'music/m1/BombMan.mp3'
+    },
+    {
+        img : 'images/m1.png',
+        name : 'Cut Man',
+        artist : 'Megaman 1',
+        music : 'music/m1/CutMan.mp3'
+    },
+    {
+        img : 'images/m1.png',
+        name : 'Elec Man',
+        artist : 'Megaman 1',
+        music : 'music/m1/ElecMan.mp3'
+    },
+    {
+        img : 'images/m1.png',
+        name : 'Fire Man',
+        artist : 'Megaman 1',
+        music : 'music/m1/FireMan.mp3'
+    },
+    {
+        img : 'images/m1.png',
+        name : 'Guts Man',
+        artist : 'Megaman 1',
+        music : 'music/m1/GutsMan.mp3'
+    },
+    {
+        img : 'images/m1.png',
+        name : 'Ice Man',
+        artist : 'Megaman 1',
+        music : 'music/m1/IceMan.mp3'
+    },
+    {
+        img : 'images/m2.png',
+        name : 'Air Man',
+        artist : 'Megaman 2',
+        music : 'music/m2/AirMan.mp3'
+    },
+    {
+        img : 'images/m2.png',
+        name : 'Bubble Man',
+        artist : 'Megaman 2',
+        music : 'music/m2/BubbleMan.mp3'
+    },
+    {
+        img : 'images/m2.png',
+        name : 'Crash Man',
+        artist : 'Megaman 2',
+        music : 'music/m2/CrashMan.mp3'
+    },
+    {
+        img : 'images/m2.png',
+        name : 'Flash Man',
+        artist : 'Megaman 2',
+        music : 'music/m2/FlashMan.mp3'
+    },
+    {
+        img : 'images/m2.png',
+        name : 'Heat Man',
+        artist : 'Megaman 2',
+        music : 'music/m2/HeatMan.mp3'
+    },
+    {
+        img : 'images/m2.png',
+        name : 'Metal Man',
+        artist : 'Megaman 2',
+        music : 'music/m2/MetalMan.mp3'
+    },
+    {
+        img : 'images/m2.png',
+        name : 'Quick Man',
+        artist : 'Megaman 2',
+        music : 'music/m2/QuickMan.mp3'
+    },
+    {
+        img : 'images/m2.png',
+        name : 'Wood Man',
+        artist : 'Megaman 2',
+        music : 'music/m2/WoodMan.mp3'
+    },
+    {
+        img : 'images/m3.png',
+        name : 'Gemini Man',
+        artist : 'Megaman 3',
+        music : 'music/m3/GeminiMan.mp3'
+    },
+    {
+        img : 'images/m3.png',
+        name : 'Hard Man',
+        artist : 'Megaman 3',
+        music : 'music/m3/HardMan.mp3'
+    },
+    {
+        img : 'images/m3.png',
+        name : 'Magnet Man',
+        artist : 'Megaman 3',
+        music : 'music/m3/MagnetMan.mp3'
+    },
+    {
+        img : 'images/m3.png',
+        name : 'Needle Man',
+        artist : 'Megaman 3',
+        music : 'music/m3/NeedleMan.mp3'
+    },
+    {
+        img : 'images/m3.png',
+        name : 'Shadow Man',
+        artist : 'Megaman 3',
+        music : 'music/m3/ShadowMan.mp3'
+    },
+    {
+        img : 'images/m3.png',
+        name : 'Snake Man',
+        artist : 'Megaman 3',
+        music : 'music/m3/SnakeMan.mp3'
+    },
+    {
+        img : 'images/m3.png',
+        name : 'Spark Man',
+        artist : 'Megaman 3',
+        music : 'music/m3/SparkMan.mp3'
+    },
+    {
+        img : 'images/m3.png',
+        name : 'Top Man',
+        artist : 'Megaman 3',
+        music : 'music/m3/TopMan.mp3'
+    },
+    // {
+    //     img : 'images/m4.png',
+    //     name : 'Bright Man',
+    //     artist : 'Megaman 4',
+    //     music : 'music/m4/BrightMan.mp3'
+    // },
+    // {
+    //     img : 'images/m4.png',
+    //     name : 'Dive Man',
+    //     artist : 'Megaman 4',
+    //     music : 'music/m4/DiveMan.mp3'
+    // },
+    // {
+    //     img : 'images/m4.png',
+    //     name : 'Drill Man',
+    //     artist : 'Megaman 4',
+    //     music : 'music/m4/DrillMan.mp3'
+    // },
+    // {
+    //     img : 'images/m4.png',
+    //     name : 'Dust Man',
+    //     artist : 'Megaman 4',
+    //     music : 'music/m4/DustMan.mp3'
+    // },
+    // {
+    //     img : 'images/m4.png',
+    //     name : 'Pharaoh Man',
+    //     artist : 'Megaman 4',
+    //     music : 'music/m4/PharaohMan.mp3'
+    // },
+    // {
+    //     img : 'images/m4.png',
+    //     name : 'Ring Man',
+    //     artist : 'Megaman 4',
+    //     music : 'music/m4/RingMan.mp3'
+    // },
+    // {
+    //     img : 'images/m4.png',
+    //     name : 'Skull Man',
+    //     artist : 'Megaman 4',
+    //     music : 'music/m4/SkullMan.mp3'
+    // },
+    // {
+    //     img : 'images/m4.png',
+    //     name : 'Toad Man',
+    //     artist : 'Megaman 4',
+    //     music : 'music/m4/ToadMan.mp3'
+    // },
+    // {
+    //     img : 'images/m5.jpg',
+    //     name : 'Charge Man',
+    //     artist : 'Megaman 5',
+    //     music : 'music/m5/ChargeMan.mp3'
+    // },
+    // {
+    //     img : 'images/m5.jpg',
+    //     name : 'Crystal Man',
+    //     artist : 'Megaman 5',
+    //     music : 'music/m5/CrystalMan.mp3'
+    // },
+    // {
+    //     img : 'images/m5.jpg',
+    //     name : 'Gravity Man',
+    //     artist : 'Megaman 5',
+    //     music : 'music/m5/GravityMan.mp3'
+    // },
+    // {
+    //     img : 'images/m5.jpg',
+    //     name : 'Gyro Man',
+    //     artist : 'Megaman 5',
+    //     music : 'music/m5/GyroMan.mp3'
+    // },
+    // {
+    //     img : 'images/m5.jpg',
+    //     name : 'Napalm Man',
+    //     artist : 'Megaman 5',
+    //     music : 'music/m5/NapalmMan.mp3'
+    // },
+    // {
+    //     img : 'images/m5.jpg',
+    //     name : 'Star Man',
+    //     artist : 'Megaman 5',
+    //     music : 'music/m5/StarMan.mp3'
+    // },
+    // {
+    //     img : 'images/m5.jpg',
+    //     name : 'Stone Man',
+    //     artist : 'Megaman 5',
+    //     music : 'music/m5/StoneMan.mp3'
+    // },
+    // {
+    //     img : 'images/m5.jpg',
+    //     name : 'Wave Man',
+    //     artist : 'Megaman 5',
+    //     music : 'music/m5/WaveMan.mp3'
+    // },
+    // {
+    //     img : 'images/m6.png',
+    //     name : 'Blizzard Man',
+    //     artist : 'Megaman 6',
+    //     music : 'music/m6/BlizzardMan.mp3'
+    // },
+    // {
+    //     img : 'images/m6.png',
+    //     name : 'Centaur Man',
+    //     artist : 'Megaman 6',
+    //     music : 'music/m6/CentaurMan.mp3'
+    // },
+    // {
+    //     img : 'images/m6.png',
+    //     name : 'Flame Man',
+    //     artist : 'Megaman 6',
+    //     music : 'music/m6/FlameMan.mp3'
+    // },
+    // {
+    //     img : 'images/m6.png',
+    //     name : 'Knight Man',
+    //     artist : 'Megaman 6',
+    //     music : 'music/m6/KnightMan.mp3'
+    // },
+    // {
+    //     img : 'images/m6.png',
+    //     name : 'Plant Man',
+    //     artist : 'Megaman 6',
+    //     music : 'music/m6/PlantMan.mp3'
+    // },
+    // {
+    //     img : 'images/m6.png',
+    //     name : 'Tomahawk Man',
+    //     artist : 'Megaman 6',
+    //     music : 'music/m6/TomahawkMan.mp3'
+    // },
+    // {
+    //     img : 'images/m6.png',
+    //     name : 'Wind Man',
+    //     artist : 'Megaman 6',
+    //     music : 'music/m6/WindMan.mp3'
+    // },
+    // {
+    //     img : 'images/m6.png',
+    //     name : 'Yamato Man',
+    //     artist : 'Megaman 6',
+    //     music : 'music/m6/YamatoMan.mp3'
+    // },
+    // {
+    //     img : 'images/m9.jpg',
+    //     name : 'Concrete Man',
+    //     artist : 'Megaman 9',
+    //     music : 'music/m9/ConcreteMan.mp3'
+    // },
+    // {
+    //     img : 'images/m9.jpg',
+    //     name : 'Galaxy Man',
+    //     artist : 'Megaman 9',
+    //     music : 'music/m9/GalaxyMan.mp3'
+    // },
+    // {
+    //     img : 'images/m9.jpg',
+    //     name : 'Hornet Man',
+    //     artist : 'Megaman 9',
+    //     music : 'music/m9/HornetMan.mp3'
+    // },
+    // {
+    //     img : 'images/m9.jpg',
+    //     name : 'Jewel Man',
+    //     artist : 'Megaman 9',
+    //     music : 'music/m9/JewelMan.mp3'
+    // },
+    // {
+    //     img : 'images/m9.jpg',
+    //     name : 'Magma Man',
+    //     artist : 'Megaman 9',
+    //     music : 'music/m9/MagmaMan.mp3'
+    // },
+    // {
+    //     img : 'images/m9.jpg',
+    //     name : 'Plug Man',
+    //     artist : 'Megaman 9',
+    //     music : 'music/m9/PlugMan.mp3'
+    // },
+    // {
+    //     img : 'images/m9.jpg',
+    //     name : 'Splash Woman',
+    //     artist : 'Megaman 9',
+    //     music : 'music/m9/SplashWoman.mp3'
+    // },
+    // {
+    //     img : 'images/m9.jpg',
+    //     name : 'Tornado Man',
+    //     artist : 'Megaman 9',
+    //     music : 'music/m9/TornadoMan.mp3'
+    // },
+    // {
+    //     img : 'images/m10.jfif',
+    //     name : 'Blade Man',
+    //     artist : 'Megaman 10',
+    //     music : 'music/m10/BladeMan.mp3'
+    // },
+    // {
+    //     img : 'images/m10.jfif',
+    //     name : 'Chill Man',
+    //     artist : 'Megaman 10',
+    //     music : 'music/m10/ChillMan.mp3'
+    // },
+    // {
+    //     img : 'images/m10.jfif',
+    //     name : 'Commando Man',
+    //     artist : 'Megaman 10',
+    //     music : 'music/m10/CommandoMan.mp3'
+    // },
+    // {
+    //     img : 'images/m10.jfif',
+    //     name : 'Nitro Man',
+    //     artist : 'Megaman 10',
+    //     music : 'music/m10/NitroMan.mp3'
+    // },
+    // {
+    //     img : 'images/m10.jfif',
+    //     name : 'Pump Man',
+    //     artist : 'Megaman 10',
+    //     music : 'music/m10/PumpMan.mp3'
+    // },
+    // {
+    //     img : 'images/m10.jfif',
+    //     name : 'Sheep Man',
+    //     artist : 'Megaman 10',
+    //     music : 'music/m10/SheepMan.mp3'
+    // },
+    // {
+    //     img : 'images/m10.jfif',
+    //     name : 'Solar Man',
+    //     artist : 'Megaman 10',
+    //     music : 'music/m10/SolarMan.mp3'
+    // },
+    // {
+    //     img : 'images/m10.jfif',
+    //     name : 'Strike Man',
+    //     artist : 'Megaman 10',
+    //     music : 'music/m10/StrikeMan.mp3'
+    // },
+    // {
+    //     img : 'images/mxs.jfif',
+    //     name : 'Blanka',
+    //     artist : 'Megaman xs',
+    //     music : 'music/mxs/Blanka.mp3'
+    // },
+    // {
+    //     img : 'images/mxs.jfif',
+    //     name : 'Boxer',
+    //     artist : 'Megaman xs',
+    //     music : 'music/mxs/Boxer.mp3'
+    // },
+    // {
+    //     img : 'images/mxs.jfif',
+    //     name : 'Chun Li',
+    //     artist : 'Megaman xs',
+    //     music : 'music/mxs/Chun Li.mp3'
+    // },
+    // {
+    //     img : 'images/mxs.jfif',
+    //     name : 'Claw',
+    //     artist : 'Megaman xs',
+    //     music : 'music/mxs/Claw.mp3'
+    // },
+    // {
+    //     img : 'images/mxs.jfif',
+    //     name : 'Dhalsim',
+    //     artist : 'Megaman xs',
+    //     music : 'music/mxs/Dhalsim.mp3'
+    // },
+    // {
+    //     img : 'images/mxs.jfif',
+    //     name : 'Rose',
+    //     artist : 'Megaman xs',
+    //     music : 'music/mxs/Rose.mp3'
+    // },
+    // {
+    //     img : 'images/mxs.jfif',
+    //     name : 'Ryu',
+    //     artist : 'Megaman xs',
+    //     music : 'music/mxs/Ryu.mp3'
+    // },
+    // {
+    //     img : 'images/mxs.jfif',
+    //     name : 'Uriel',
+    //     artist : 'Megaman xs',
+    //     music : 'music/mxs/Uriel.mp3'
+    // },
+];
